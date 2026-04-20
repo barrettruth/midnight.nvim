3744a70ef1eef923932bae85f64c1286bc89aca7
Barrett Ruth <br.barrettruth@gmail.com>
2026-04-20 15:53:09 -0400

    ci: make just the task source of truth

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
       devShells = forEachSystem (pkgs: {
         default = pkgs.mkShell {
           packages = [
+            pkgs.just
             pkgs.prettier
             pkgs.stylua
             pkgs.selene
@@ -34,6 +35,7 @@
 
         ci = pkgs.mkShell {
           packages = [
+            pkgs.just
             pkgs.prettier
             pkgs.stylua
             pkgs.neovim
